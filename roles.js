826b33f57c66666556b859d8ebb2ea5dbb8379c5
Heark <hearkofsorcerey@gmail.com>
2014-11-17 11:04:08 -0500

    Update roles.js

--- roles.js
+++ roles.js
@@ -4,4 +4,8 @@
 
 */
 
-var roles = ["Jester", "",];
+var roles = ["Jester", "Bodyguard", "Doctor", "Investigator", "Villager", "Godfather", "Mafioso", "Blackmailer"];
+var player = function(role, description){
+ role = roles[Math.floor(Math.random()*roles.length)];
+ description = this.description;
+}
